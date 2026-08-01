ea3f413ad9742af8ab5f5d179eae6a94238666f2
Ademar Couto Guimarães <84032449+ademarsoad@users.noreply.github.com>
2026-08-01 18:20:58 -0300

    aprimorado o search e adicionado algumas imagens

--- explorarwhiskys.php
+++ explorarwhiskys.php
@@ -2,14 +2,18 @@
 require('controllers/conexao.php');
 $abrirfavoritos = "";
 session_start();
+
 if (isset($_SESSION['usuario_id'])) {
     echo "Usuario " . $_SESSION['usuario_nome'] . " Logado ";
     $abrirfavoritos = "mostrarfavoritos";
 }
+if(isset($_POST['search'])) {$nomepesquisa = $_POST['search']; } else {$nomepesquisa = ""; };
 
 
-$stmt = $conn->prepare("SELECT w.nome, w.descricao, t.tipo from whiskys w
-inner join tipowhisky t on w.tipo = t.id");
+$stmt = $conn->prepare("SELECT w.nome, w.descricao, w.imagemwhisky, t.tipo from whiskys w
+inner join tipowhisky t on w.tipo = t.id WHERE w.nome like :nome or t.tipo like :nome" );
+$stmt->bindValue(":nome", "%$nomepesquisa%");
+
 $stmt->execute();
 
 $listaehisky = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -25,15 +29,15 @@ $listaehisky = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>Whiskys</title>
 
     <style>
-        
+
     </style>
 
 </head>
 
 <body>
     <?php include('view/header.php'); ?>
-    <form action="">
-        <div id="search">
+    <form action="" method="post">
+        <div id="search" >
             <i class="fa-solid fa-magnifying-glass"></i>
             <input type="text" name="search" id="search" placeholder="Pesquise seu Whisky">
         </div>
@@ -47,7 +51,7 @@ $listaehisky = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <div class="whisky-card">
                 <h2><?php echo $whisky['nome']; ?></h2>
                 <p class="whiskycardtipo"><?php echo $whisky['tipo']; ?></p>
-                <img src="src/img/<?php echo $whisky['nome'] . ".jpg" ?>" alt="<?php echo $whisky['nome'] . " " . $whisky['tipo']; ?>">
+                <img src="src/img/<?php echo $whisky['imagemwhisky']; ?>" alt="<?php echo $whisky['nome'] . " " . $whisky['tipo']; ?>">
 
                 <button>Adicionar aos Favoritos</button>
             </div>
